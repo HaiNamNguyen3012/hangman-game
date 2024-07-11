5c5b1ce54f4d00af328d1cfdefa646c6c7d5c10f
Hai Nam Nguyen <hainam.nguyen03122000@gmail.com>
2024-07-11 15:28:43 +0700

    hang man game

---
+++ src/main/java/com/codecool/hangman/Application.java
@@ -0,0 +1,18 @@
+package com.codecool.hangman;
+
+/**
+ * Top class which starts the Application.
+ */
+class Application {
+
+    /**
+     * Main method of the program - initialize Game and call start method on it.
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        Game game = new Game();
+        game.start();
+
+    }
+}
